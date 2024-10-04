7af4dc81b7ee66f34d266a6875645643a22da739
PremPremPremPrem <prem.sy1952@gmail.com>
2024-10-04 11:28:55 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+    <div class="bg-zinc-950 w-screen h-screen text-white overflow-hidden ease-[cubic-bezier(.33,1.15,.49,1.44)]">
+        <RouterView />
+    </div>
+</template>
